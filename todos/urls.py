591a2a91a392e32825d6bfcb4d12df1d6156c535
Samuel-Adisi <samueladisi734@gmail.com>
2026-06-14 01:02:39 -0700

    sss

--- todos/urls.py
+++ todos/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
 
-app_name='todos'
+app_name = 'todos'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:todo_id>/delete', views.delete, name='delete'),
-    path('<int:todo_id>/update', views.update, name='update'),
-    path('add/', views.add, name='add')
+    path('add/', views.add, name='add'),
+    path('delete/<int:todo_id>/', views.delete, name='delete'),
+    path('update/<int:todo_id>/', views.update, name='update'),
+    path('rename/<int:todo_id>/', views.rename, name='rename'),
 ]
