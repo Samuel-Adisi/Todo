5dbf6d00d2572e2f6143c8f7abcec11a9b17ac21
Samuel-Adisi <samueladisi734@gmail.com>
2026-06-14 11:46:43 -0700

    first commit

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -78,7 +78,7 @@ import dj_database_url
 
 
 DATABASES = {
-    'default': dj_database_url.config(default='postgresql://neondb_owner:npg_3kVDwQR2IAxn@ep-soft-lake-adfu3jyu-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require')
+    'default': dj_database_url.config(default='postgresql://neondb_owner:npg_REW8KAcYV4UN@ep-dark-frost-addt7gzp-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require')
 }
 
 # Password validation
